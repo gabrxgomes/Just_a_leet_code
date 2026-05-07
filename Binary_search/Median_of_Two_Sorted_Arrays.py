e9c7d82ca0ddbe19c506878043b21e625502227a
Gabriel Nascimento Gomes <gabrxgomes@Gabriels-MacBook-Air.local>
2026-05-07 12:21:03 -0300

    sending the source code and explanation for leetcode Median of two sorted arrays

---
+++ Binary_search/Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        #for this leetcode i need merge the two
+        #arrays and take the middle number using slice? Maybe
+
+        #merged = sorted(nums1 + nums2)
+
+        #well the first thing is to define an variable for receiving the merged array, then containing (nums1 + nums2)
+        
+
+
+        
+
+
