58e410cad5371f00418a39d90061a404241da227
Gabriel Nascimento Gomes <gabrxgomes@Gabriels-MacBook-Air.local>
2026-05-07 23:29:50 -0300

    sending the solution for Median of two sorted arrays

--- Binary_search/Median_of_Two_Sorted_Arrays.py
+++ Binary_search/Median_of_Two_Sorted_Arrays.py
@@ -11,7 +11,16 @@ class Solution(object):
         #merged = sorted(nums1 + nums2)
 
         #well the first thing is to define an variable for receiving the merged array, then containing (nums1 + nums2)
+        
+        
         merged = sorted(nums1 + nums2)
+        mergedlen = len(merged)
+
+
+        if mergedlen % 2 == 0:
+            return (merged[mergedlen//2 - 1] + merged[mergedlen//2]) / 2.0
+        else:
+            return merged[mergedlen//2]
 
 
         
